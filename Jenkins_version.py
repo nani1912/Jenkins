33ec639a555e103e6cf1ab24fd7ed22ca4168530
nani1912 <rangap1912@gmail.com>
2018-06-05 05:15:35 -0500

    Create Jenkins_version.py

---
+++ Jenkins_version.py
@@ -0,0 +1,9 @@
+## This is the script for checking Jenkins version 
+
+
+import jenkins
+
+server = jenkins.Jenkins('http://localhost:8080', username='myuser', password='mypassword')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
